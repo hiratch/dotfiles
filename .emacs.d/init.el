516f7e2c849fdf7fd772285aa5de124ee6695005
Kazuhiro Hiramoto <k-hiramoto@morphoinc.com>
2019-04-17 16:28:55 +0900

    add this for avoiding an error

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -306,3 +306,10 @@
 ;; [todo] add
 ;; yasnippet, rtags, irony, flycheck
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
