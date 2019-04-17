bd3657a8104da162ad7332d3fb8d84ece374f12c
Kazuhiro Hiramoto <k-hiramoto@morphoinc.com>
2019-04-17 17:53:17 +0900

    add the changes from the first run-time

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -311,5 +311,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (magit transient git-commit with-editor dash async popwin rainbow-delimiters quelpa-use-package use-package bind-key quelpa))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
