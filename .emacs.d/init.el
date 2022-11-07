f3ea2e670de9a3d31c914988107671c9f7092231
K.Hiramoto <wanpac@gmail.com>
2022-11-07 09:59:05 +0900

    add typescript-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -460,6 +460,9 @@
             )
   )
 
+;;; tsx (typescript)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+
 ;;; flycheck-rust
 (use-package flycheck-rust
   :init
@@ -485,7 +488,7 @@
 ")))))
  '(irony-additional-clang-options '("-std=c++11"))
  '(package-selected-packages
-   '(matlab-mode gnu-elpa-keyring-update magit transient git-commit with-editor dash async popwin rainbow-delimiters quelpa-use-package use-package bind-key quelpa)))
+   '(typescript-mode flycheck-rust flycheck-rtags flycheck company-rtags company-irony company matlab-mode gnu-elpa-keyring-update magit transient git-commit with-editor dash async popwin rainbow-delimiters quelpa-use-package use-package bind-key quelpa)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
