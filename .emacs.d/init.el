3abf53c794d5f5a36a15c966a1b8d272f4193aa5
Kazuhiro Hiramoto <hiramoto.kazuhiro@nabla-mobility.com>
2025-10-16 18:39:09 +0900

    fix some problems

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,19 +3,15 @@
 ;;; ==========================================================================
 ;;; Section 1: Bootstrap - パッケージ管理システムの準備
 ;;; ==========================================================================
-;; Emacs 27.1以降のLisp readerのバグを回避
 (setq read-process-output-max (* 1024 1024))
 
-;; パッケージ取得先リポジトリを設定
 (setq package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
         ("org"   . "https://orgmode.org/elpa/")))
 
-;; package.elを初期化
 (package-initialize)
 
-;; Quelpaのインストール (なければ)
 (unless (package-installed-p 'quelpa)
   (with-temp-buffer
     (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
@@ -23,14 +19,11 @@
     (quelpa-self-upgrade)))
 (require 'quelpa)
 
-;; use-packageのインストール (なければ)
-;; quelpa-use-packageもここで導入する
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 
-;; quelpaをuse-packageで使えるようにする
 (quelpa
  '(quelpa-use-package
    :fetcher git
@@ -44,30 +37,24 @@
 
 ;; UI設定 --------------------------------------------------------------------
 (setq-default
- inhibit-startup-message t      ; スタートアップメッセージを非表示
- line-number-mode t             ; 行番号をモードラインに表示
- column-number-mode t           ; 桁番号をモードラインに表示
- indent-tabs-mode nil           ; タブの代わりにスペースを使用
- kill-whole-line t              ; C-kで行全体をキル
- visible-bell t                 ; ビープ音の代わりに画面をフラッシュ
- truncate-lines t               ; 行を折り返さない
- find-file-visit-truename t     ; ファイルアクセス時にシンボリックリンクを解決
- read-file-name-completion-ignore-case t) ; ファイル名補完で大文字小文字を無視
+ inhibit-startup-message t
+ line-number-mode t
+ column-number-mode t
+ indent-tabs-mode nil
+ kill-whole-line t
+ visible-bell t
+ truncate-lines t
+ find-file-visit-truename t
+ read-file-name-completion-ignore-case t)
 
-;; ツールバー、メニューバー、スクロールバーを非表示
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-
-;; 対応する括弧をハイライト
 (show-paren-mode t)
-
-;; バックアップファイルを作成しない
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq auto-save-list-file-prefix nil)
 
-;; 初期フレーム（ウィンドウ）の設定
 (setq default-frame-alist
       '((foreground-color . "gray85")
         (background-color . "gray2")
@@ -82,7 +69,6 @@
         (cursor-color . "red")
         (alpha . (90 70))))
 
-;; リージョン（選択範囲）の色
 (set-face-background 'region "forest green")
 
 ;; キーバインド --------------------------------------------------------------
@@ -91,9 +77,18 @@
 (global-set-key (kbd "M-s") 'shell)
 (global-set-key (kbd "C-x k") 'kill-current-buffer)
 
-;; windmove (Shift + 矢印でウィンドウ間を移動)
 (windmove-default-keybindings)
 (setq windmove-wrap-around t)
+
+;; 復元機能: [EOF]マークをバッファ末尾に表示 --------------------------------
+(defun set-buffer-end-mark ()
+  (let ((overlay (make-overlay (point-max) (point-max))))
+    (overlay-put overlay 'before-string #("[EOF]" 0 5 (face highlight)))
+    (overlay-put overlay 'insert-behind-hooks
+                 '((lambda (overlay after beg end &optional len)
+                     (when after
+                       (move-overlay overlay (point-max) (point-max))))))))
+(add-hook 'find-file-hooks 'set-buffer-end-mark)
 
 
 ;;; ==========================================================================
@@ -101,22 +96,58 @@
 ;;; ==========================================================================
 
 ;; 汎用ユーティリティ ----------------------------------------------------
+(use-package bind-key ;; GDB設定で利用
+  :quelpa t)
+
 (use-package popwin
   :quelpa t
   :config
   (popwin-mode 1)
   (setq popwin:popup-window-position 'bottom))
 
+;; 復元機能: rainbow-delimitersの色を強調
 (use-package rainbow-delimiters
   :quelpa t
-  :hook (prog-mode . rainbow-delimiters-mode))
+  :hook (prog-mode . rainbow-delimiters-mode)
+  :config
+  (defun rainbow-delimiters-using-stronger-colors ()
+    "Make the rainbow-delimiters faces more saturated."
+    (interactive)
+    (require 'color)
+    (cl-loop
+     for index from 1 to rainbow-delimiters-max-face-count
+     do
+     (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+      (cl-callf color-saturate-name (face-foreground face) 30))))
+  ;; 起動時に一度だけ実行
+  (add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors))
 
-;; CUA-mode (矩形選択)
+;; 復元機能: cua-modeの連番挿入機能を拡張
 (use-package cua-base
   :quelpa t
   :config
   (cua-mode 1)
-  (setq cua-enable-cua-keys nil)) ; C-x, C-cなどを上書きしない
+  (setq cua-enable-cua-keys nil) ; C-x, C-cなどを上書きしない
+  ;; 連番挿入の拡張
+  (defadvice cua-sequence-rectangle (around my-cua-sequence-rectangle activate)
+    "連番を挿入するとき、紫のところの文字を上書きしないで左にずらす."
+    (interactive
+     (list (if current-prefix-arg
+               (prefix-numeric-value current-prefix-arg)
+             (string-to-number
+              (read-string "Start value: (0) " nil nil "0")))
+           (string-to-number
+            (read-string "Increment: (1) " nil nil "1"))
+           (read-string (concat "Format: (" cua--rectangle-seq-format ") "))))
+    (if (= (length format) 0)
+        (setq format cua--rectangle-seq-format)
+      (setq cua--rectangle-seq-format format))
+    (cua--rectangle-operation 'clear nil t 1 nil
+       '(lambda (s e l r)
+           (kill-region s e)
+           (insert (format format first))
+           (yank)
+           (setq first (+ first incr))))))
 
 ;; Magit (Gitクライアント)
 (use-package magit
@@ -126,36 +157,44 @@
 (use-package compat
   :quelpa (compat :fetcher github :repo "emacs-compat/compat"))
 
+;; 復元機能: topsy (which-functionの代替)
+(use-package topsy
+  :quelpa (topsy :fetcher github :repo "alphapapa/topsy.el")
+  :hook ((prog-mode . topsy-mode)
+         (magit-section-mode . topsy-mode))
+  :config
+  ;; topsyをヘッダーラインに表示するための設定
+  (delete (assoc 'which-func-mode mode-line-format) mode-line-format)
+  (setq-default header-line-format '(which-func-mode ("" which-func-format))))
+
+
 ;; コード補完 & スニペット -----------------------------------------------
 (use-package yasnippet
   :quelpa t
   :hook (after-init . yas-global-mode)
   :config
-  ;; スニペットディレクトリの指定
   (setq yas-snippet-dirs (list "~/.emacs.d/snippets" "~/.emacs.d/mysnippets"))
-  ;; 以前のinit.elにあったキーバインドを復元
   (define-key yas-minor-mode-map (kbd "C-c C-s") 'yas-insert-snippet)
   (define-key yas-minor-mode-map (kbd "C-c C-n") 'yas-new-snippet)
   (define-key yas-minor-mode-map (kbd "C-c C-v") 'yas-visit-snippet-file)
-  ;; companyとの連携のため、TABキーのバインドはここでは無効化しておく
   (define-key yas-minor-mode-map (kbd "TAB") nil)
   (define-key yas-minor-mode-map (kbd "<tab>") nil))
 
-(use-package yasnippet-snippets
-  :quelpa t
-  :after yasnippet)
-
 (use-package company
   :quelpa t
-  :after yasnippet ; yasnippetの読み込み後に設定する
+  :after yasnippet
   :hook (after-init . global-company-mode)
   :bind (("C-M-i" . company-complete))
   :config
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
-  ;; companyの設定ブロックに移動
   (add-to-list 'company-backends 'company-yasnippet))
+
+(use-package yasnippet-snippets
+  :quelpa t
+  :after yasnippet)
+
 
 ;; 静的解析 (Flycheck) ----------------------------------------------------
 (use-package flycheck
@@ -163,7 +202,13 @@
   :hook (after-init . global-flycheck-mode))
 
 
-;; C / C++ 開発環境 -------------------------------------------------------
+;; 言語別設定 -------------------------------------------------------------
+
+;; 復元機能: C言語モードで末尾の空白を表示
+(use-package cc-mode
+  :ensure nil ; ビルトインパッケージ
+  :hook (c-mode-common-hook . (lambda () (setq show-trailing-whitespace t))))
+
 (use-package irony
   :quelpa t
   :hook ((c-mode . irony-mode)
@@ -190,8 +235,6 @@
   :hook ((c-mode . flycheck-rtags-setup)
          (c++-mode . flycheck-rtags-setup)))
 
-
-;; Rust 開発環境 ----------------------------------------------------------
 (use-package rust-mode
   :quelpa t
   :config
@@ -207,8 +250,6 @@
   :quelpa t
   :hook (rust-mode . flycheck-rust-setup))
 
-
-;; その他モード設定 -------------------------------------------------------
 (use-package csharp-mode
   :quelpa t
   :mode "\\.cs\\'")
@@ -217,11 +258,22 @@
   :quelpa t
   :mode "\\.tsx\\'")
 
+;; 復元機能: GDB関連の詳細な設定
+(use-package gud
+  :ensure nil ; ビルトインパッケージ
+  :init
+  (bind-keys :map mode-specific-map
+             ("C-c C-SPC" . gud-break))
+  (add-hook 'gdb-mode-hook '(lambda () (gud-tooltip-mode t)))
+  :config
+  (setq gdb-many-windows t)
+  (setq gdb-use-serapate-io-buffer t)
+  (setq gud-tooltip-echo-area nil))
+
+
 ;;; ==========================================================================
 ;;; Section 4: Custom Set Variables - カスタマイズUIによる設定
 ;;; ==========================================================================
-;; このセクションはcustomize UIによって自動的に編集されます。
-;; 手動で編集せず、設定は各use-packageブロックに移動していくのが理想です。
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -229,11 +281,14 @@
  ;; If there is more than one, they won't work right.
  '(flycheck-display-errors-delay 0.5)
  '(flycheck-display-errors-function
-   (lambda (errors)
-     (let ((messages (mapcar #'flycheck-error-message errors)))
-       (popup-tip (mapconcat 'identity messages "\12")))))
- '(irony-additional-clang-options '("-std=c++11"))
- '(package-selected-packages nil))
+   (lambda
+     (errors)
+     (let
+         ((messages
+           (mapcar #'flycheck-error-message errors)))
+       (popup-tip
+        (mapconcat 'identity messages "\12")))))
+ '(irony-additional-clang-options '("-std=c++11")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -243,7 +298,5 @@
  )
 
 ;; OPAM (OCaml Package Manager) の設定はそのまま残します
-;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 (when (file-exists-p "~/.emacs.d/opam-user-setup.el")
   (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el"))
-;; ## end of OPAM user-setup addition for emacs / base ## keep this line
