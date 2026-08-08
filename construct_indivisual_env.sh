766dc2b3d96203d4c7c50d0a0ecaf97a80d7b305
Kazuhiro Hiramoto <hiramoto.kazuhiro@nabla-mobility.com>
2026-08-08 11:19:49 +0900

    introduce skills

--- construct_indivisual_env.sh
+++ construct_indivisual_env.sh
@@ -55,7 +55,7 @@ ln -sfn "$HOME/dotfiles/Brewfile" "$HOME/Brewfile"
 mkdir -p "$HOME/.claude"
 #ln -sf "$HOME/dotfiles/.claude/keybindings.json" "$HOME/.claude/keybindings.json"
 ln -sfn "$HOME/dotfiles/.claude/CLAUDE.md" "$HOME/.claude/CLAUDE.md"
-ln -sfn "$HOME/dotfiles/.claude/commands" "$HOME/.claude/commands"
+ln -sfn "$HOME/dotfiles/.claude/skills" "$HOME/.claude/skills"
 ln -sfn "$HOME/dotfiles/.claude/settings.json" "$HOME/.claude/settings.json"
 
 echo "ユーザー環境のセットアップが完了しました！"
